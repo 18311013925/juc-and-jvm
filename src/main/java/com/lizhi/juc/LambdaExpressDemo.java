19e23ea285ebc01ba7122fb8fa4c4239a674e06a
lizhi <949168418@qq.com>
2020-03-19 23:12:13 +0800

    创建一个juc和jvm 的项目，方便学习

---
+++ src/main/java/com/lizhi/juc/LambdaExpressDemo.java
@@ -0,0 +1,51 @@
+package com.lizhi.juc;
+
+/**
+ * 1、函数式编程
+ * 口诀：拷贝小括号 ， 写死右箭头， 落地大括号
+ *
+ * lambda 表达式就是解决内部类
+ */
+
+interface Foo{
+    void sayHello();
+}
+@FunctionalInterface
+interface Foo2{
+    int add(int x , int y);
+    default void sayHello(){
+        System.out.println("hello");
+    }
+}
+
+public class LambdaExpressDemo {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            new Thread("threadName").start();
+        }
+
+    }
+
+    /*
+    * lambda 学习
+    * */
+    private static void lambdaExpressLearn() {
+        /**
+         * 匿名内部类，代码一坨
+         */
+        /*Foo foo = new Foo() {
+            public void sayHello() {
+                System.out.println("hello");
+            }
+        };
+        foo.sayHello();*/
+//        lambda
+        Foo foo = () -> {
+            System.out.println("hello");
+        };
+        foo.sayHello();
+    }
+
+
+}
